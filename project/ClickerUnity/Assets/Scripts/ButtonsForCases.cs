ba83b119d3f56bb37838acd4c667077fac66a8fe
vetrandir <vetrandir@gmail.com>
2018-12-04 21:11:33 +0300

    Cases work!

---
+++ project/ClickerUnity/Assets/Scripts/ButtonsForCases.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
+
+public class ButtonsForCases : MonoBehaviour {
+    Random realRnd = new Random();
+    int min = 10, max = 10000;
+    public Text text;
+    private void Awake()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Debug.Log("случайные числа в диапазоне [" + min + "," + max + "]");
+        Debug.Log("Число " + "= " + realRnd.Next(min, max));
+        int x = realRnd.Next(min, max);
+        text.text = "Вы получили " + x + " деталей, потратьте же их куда-нибудь!";
+    }
+}
---
+++ project/TestButtons/Assets/HUDmanager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDmanager : MonoBehaviour {
+    public Text txt;
+	// Use this for initialization
+	void Start () {
+        txt.text = "72";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
